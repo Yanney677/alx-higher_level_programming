d7ea8021e025befc7a65416d3869b8142467a9e7
Yanney677 <w.yanney1991@gmail.com>
2023-08-29 02:10:07 +0000

    Elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
 
+def safe_print_list(my_list=[], x=0):
     result = 0
     for i in range(x):
         try:
@@ -11,4 +11,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return (result)
-
