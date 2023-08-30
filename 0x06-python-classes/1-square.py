07ce783226130962c7d6e8dcd60d71dfbe64e8a1
Yanney677 <w.yanney1991@gmail.com>
2023-08-30 05:24:05 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define the class"""
+
+
+class Square:
+    """implementation of the class square"""
+
+    def __init__(self, size):
+        """initialisation of the new square
+
+        Args:
+            size (int): size of the new square
+        """
+        self.__size = size
