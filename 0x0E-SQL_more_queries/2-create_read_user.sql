c03b17f0a8e75143f4e40e5c99a2476b264297c1
Yanney677 <w.yanney1991@gmail.com>
2023-10-19 14:42:55 -0700

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,13 +1,6 @@
--- Check if the database exists
-SELECT SCHEMA_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = 'hbtn_0d_2';
--- If the database doesn't exist, create it
+-- Creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- Check if the user exists
-SELECT user FROM mysql.user WHERE user = 'user_0d_2';
--- If the user doesn't exist, create the user and grant SELECT privilege
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
--- Grant SELECT privilege to the user for the hbtn_0d_2 database
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
--- Apply the privileges
-FLUSH PRIVILEGES;
-
+-- Creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- Grants SELECT privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
