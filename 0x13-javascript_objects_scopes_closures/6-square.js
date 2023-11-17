9e89e7fc03f9f5a647c899dfb35854d13d682406
Yanney677 <w.yanney1991@gmail.com>
2023-11-17 01:47:22 +0000

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+ */
+
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
