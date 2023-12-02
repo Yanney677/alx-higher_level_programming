1c1c3795efba47cb5bf88bea0ca99cd175697a78
Yanney677 <w.yanney1991@gmail.com>
2023-12-02 08:41:21 +0000

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Script that fetches https://alx-intranet.hbtn.io/status
+"""script that fetches https://alx-intranet.hbtn.io/status
 """
+import requests
 
 if __name__ == "__main__":
-    import requests
-    
-    sew = requests.get("https://intranet.hbtn.io/status")
+    sew = requests.get('https://intranet.hbtn.io/status')
     print("Body response")
     print("\t- type: {}".format(type(sew.text)))
     print("\t- content: {}".format(sew.text))
